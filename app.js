c146a5b7424d704b9ecc4cf22399090c78b2987c
vivek <vivekcloud123@gmail.com>
2025-07-12 14:10:57 +0530

    1 commit

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ dotenv.config({
 });
 
 const mongoURI = process.env.MONGO_URI;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
 const adminSecretKey = process.env.ADMIN_SECRET_KEY || "adsasdsdfsdfsdfd";
 const userSocketIDs = new Map();
@@ -215,7 +215,7 @@ io.on("connection", (socket) => {
 
 app.use(errorMiddleware);
 
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port} in ${envMode} Mode`);
 });
 
--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    process.env.CLIENT_URL,
+    "https://frontend-1final-kisan-seva-lfhw.vercel.app", // ✅ Your deployed frontend
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
